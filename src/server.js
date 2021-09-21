15a60cb6c67604a3b6d0a01f02ff4065ef1284a0
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-09-21 04:06:42 +0100

    updating

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import UserRoute from './Routes/UserRoute.js';
+import ServiceRoute from './Routes/ServiceRoute.js';
+import ReviewRoute from './Routes/ReviewRoute.js';
 
 const app = express(); 
 
@@ -15,6 +17,8 @@ app.get('/',  (req, res) => {
     });
   });
 
-app.use('/', UserRoute)
+app.use('/', UserRoute);
+app.use('/', ServiceRoute)
+app.use('/', ReviewRoute);
 
 export default app;
