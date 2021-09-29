e44cf9dfe46fcdd1538c8b6a400de03feab01cdb
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-09-29 05:55:07 +0100

    fixing

--- src/server.js
+++ src/server.js
@@ -7,7 +7,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*');
+  next();
+})
 
 app.get('/',  (req, res) => {
     res.json({
