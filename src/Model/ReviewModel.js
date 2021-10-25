22042a4b77318641f93832fbf7ff97becc8a7b3b
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-10-25 19:18:35 +0100

    remove rating

--- src/Model/ReviewModel.js
+++ src/Model/ReviewModel.js
@@ -4,11 +4,6 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema(
   {
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
-    },
     author: {
       type: String,
       default: "No Name"
