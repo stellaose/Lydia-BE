9fcb5f284a9789ac228de7626bf49e6727873e5b
Stella Oseyomon <stella.oseyomon@gmail.com>
2021-11-18 23:42:35 +0100

    checkout

--- src/Controller/CheckoutController.js
+++ src/Controller/CheckoutController.js
@@ -1,7 +1,69 @@
 import Checkout from '../Model/CheckoutModel.js';
 
 const CheckoutController = {
+    postToCheckout: async (req, res) => {
+        try {
+          const { serviceId } = req.params;
+          const { user } = req;
+          
+          let findCheckout = await Checkout.findOne({ user: user._id });
+      
+          const isAdded = findCheckout.checkout
+                                    .filter(service => service.service.toString() === serviceId)
+                                    .length > 0;
+          
+      console.log(isAdded, "the item that exists") 
+          if (isAdded) {
+            return res.
+                    status(401)
+                    .json("Service already exists in cart");
+          }
 
+            await findCheckout.save();
+            return res
+                    .status(200)
+                    .json({
+                         message: "success", 
+                         data: findCheckout
+                        })
+                    .end();
+         
+          
+        } catch (err) {
+          console.log(err);
+          return res
+                .status(400)
+                .json({ 
+                        message: `Invalid request hmm` 
+                    })
+                .end();
+        }
+    },
+
+    getCheckoutService: async (req, res) => {
+        try {
+          
+          const findCheckoutItems = await Checkout
+                                            .find()
+                                            .populate('checkout.dish');
+          console.log(findCheckoutItems)
+          return res
+                .status(200)
+                .json({ 
+                    message: 'success', 
+                    data: findCheckoutItems 
+                })
+                .end();
+        } 
+        catch (err) {
+          return res
+                    .status(400)
+                    .json({ 
+                        message: `Invalid request` 
+                    })
+                    .end();
+        }
+    }
 }
 
 export default CheckoutController;
--- src/Routes/CheckoutRoute.js
+++ src/Routes/CheckoutRoute.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 
 import CheckoutController from '../Controller/CheckoutController.js';
-import Auth from '../Middleware/Auth.js';
+// import Auth from '../Middleware/Auth.js';
 
 let router = Router();
 
 // router.get('/list', CheckoutController.getServices)
 
-// router.get("/:userId", CheckoutController.getService);
-// router.post("/:serviceId",  CheckoutController.postService);
+router.get("/:userId", CheckoutController.getCheckoutService);
+router.post("/:serviceId",  CheckoutController.postToCheckout);
 
 // router.delete("/:serviceId", Auth, CheckoutController.deleteService);
 
